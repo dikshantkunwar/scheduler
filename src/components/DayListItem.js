1ae8a8dfa95a8b35c68ac3899a0e3914851355d2
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-13 18:52:23 -0500

    updated daylistitem with props and added tests for daylistitems

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "components/DayListItem.scss"
 const classNames = require('classnames');
 
 export default function DayListItem(props) {
+
+  const dayListClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": (props.spots === 0)
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name) }>
+    <li className = {dayListClass} onClick={() => props.setDay(props.name) }>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
