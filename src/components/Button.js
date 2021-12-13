c5f7c5d7eb908daa313662820f3d7c8bc2368f7c
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-13 18:01:05 -0500

    created different button types using storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
-
 import "components/Button.scss";
+var classNames = require('classnames');
 
 export default function Button(props) {
-   return <></>;
+   
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   })
+
+   return (
+      <button 
+         disabled = {props.disabled}
+         className= {buttonClass}
+         onClick= {props.onClick} 
+      > 
+         {props.children} 
+      </button>
+   );
 }
