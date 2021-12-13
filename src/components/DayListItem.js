ccf3ebdc0dddb974e9748d15233c2ecdf4855584
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-13 18:40:16 -0500

    added daylistitems props and sass styles

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,11 @@
+import React from "react";
+const classNames = require('classnames');
+
 export default function DayListItem(props) {
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name) }>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   )
 }
