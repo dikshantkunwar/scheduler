210898442d85c27d4d7a0cfb425fe57e9b4905ba
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-13 18:22:08 -0500

    created daylist and daylistitem components and added stories for those componenets

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "components/Button.scss";
-var classNames = require('classnames');
+const classNames = require('classnames');
 
 export default function Button(props) {
    
    const buttonClass = classNames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
-   })
+   });
 
    return (
       <button 
