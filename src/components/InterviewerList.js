d9c215403c8bfc3103f93786ca85319dbe1f7f6d
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-19 15:53:34 -0500

    fixed the setinterviewer prop passing down

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,7 +10,7 @@ export default function InterviewerList(props) {
       name = {interviewer.name}
       avatar = {interviewer.avatar}
       selected = {props.interviewer === interviewer.id}
-      onClick={ () => props.setInterviewer() }
+      setInterviewer = {props.setInterviewer}
       />
   })
 
