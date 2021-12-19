d98503493428370abb6e32c19429c98a1ba282e1
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-19 14:58:23 -0500

    added interviewer list and mapped them to create interviewerlist data

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,34 +4,30 @@ import 'components/InterviewerList.scss'
 
 export default function InterviewerList() {
 
-  const interviewer = {
-    id: 1,
-    name: "Sylvia Palmer",
-    avatar: "https://i.imgur.com/LpaY82x.png"
-  };
+  const interviewers = [
+    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+  ];
+
+  const interviewerList = interviewers.map( (interviewer) => {
+    return <InterviewerListItem
+      id = {interviewer.id}
+      name = {interviewer.name}
+      avatar = {interviewer.avatar}
+      />
+  })
 
   return (
     <>
       <section className="interviewers">
         <h4 className="interviewers__header text--light">Interviewer</h4>
         <ul className="interviewers__list">
-        <InterviewerListItem 
-          id = {interviewer.id}
-          name = {interviewer.name}
-          avatar = {interviewer.avatar}
-          // selected = {}
-          // setInterviewer = {}
-        />
+          {interviewerList}
         </ul>
       </section>
-
-    {/* <InterviewerListItem 
-      id = {interviewer.id}
-      name = {interviewer.name}
-      avatar = {interviewer.avatar}
-      // selected = {}
-      // setInterviewer = {}
-    /> */}
       </>
   )
 }
