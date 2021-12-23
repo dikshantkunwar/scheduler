b2708d19789653c881b293ccf3ef0018925268c0
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-23 12:37:57 -0500

    implemented the show componenet and story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+
+import "./styles.scss"
 // import Header from './Header'
 export default function Appointment() {
 
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerListItem from "components/InterviewerListItem"
 import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,3 +139,11 @@ storiesOf("Button", module)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm"/>)
     .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => (
+      <Show 
+        student="Lydia Miller-Jones" 
+        interviewer={interviewer} 
+        onEdit={action("onEdit")} 
+        onDelete={action("onDelete")} 
+      />
+    ));
