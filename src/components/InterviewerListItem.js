ef2bc64140c6ca5250abb037409eb2c93e8a240b
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-28 21:52:53 -0500

    fixed interviewer list item <li> from onChange to onClick

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -11,7 +11,7 @@ export default function InterviewerListItem(props) {
   return (
     <li 
       className={interviewerListClass} 
-      onChange = {props.setInterviewer}
+      onClick = {props.setInterviewer}
     >
       <img
         className="interviewers__item-image"
