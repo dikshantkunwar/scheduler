2e4ea09c2b4ef3320b8e587f5c2f0a5a3a5e6f60
dikshantkunwar <dikshantkunwar@gmail.com>
2022-01-03 23:24:39 -0500

    completed getAppointmentsForDay function and ran all tests for it successfully

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,37 @@
 export function getAppointmentsForDay(state, day) {
-  //... returns an array of appointments for that day
+  // returns an array of appointments for that day
+  if (state.days.length === 0 || state.days === undefined) {
+    return [];
+  }
+
+  const days = state.days.filter( (item) => {
+    if (item.name === day) {
+      return true;
+    } 
+  });
+
+  // return empty array when day not found
+  if (days.length === 0) {
+    return [];
+  }
+
+  if (days[0].appointments.length === 0 || days[0].appointments === undefined){
+    return [];
+  }
+
+  const appointments = days[0].appointments;
+  let results = [];
   
+  appointments.forEach( (appointment) => {
+    for (let appointmentVal in state.appointments) {
+      if (appointment.toString() === appointmentVal) {
+        results.push(state.appointments[appointmentVal]);
+      }
+    }
+  });
+
+  if (results.length > 0) {
+    return results;
+  }
+
 }
