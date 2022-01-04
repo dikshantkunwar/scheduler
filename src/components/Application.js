1c37df61286b0a62690c29d81d015be0d2b8c185
dikshantkunwar <dikshantkunwar@gmail.com>
2022-01-03 20:44:13 -0500

    fetching days using scheduler-api, used the useeffect hook and installed axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -62,6 +45,14 @@ const appointments = [
 ];
 
 export default function Application(props) {
+  const [days, setDate] = useState([]);
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      console.log(response.data);
+      setDate([...response.data])
+    })
+  }, [days])
+
   const [day, setDay] = useState("Monday")
   const appointmentData = appointments.map( appointment => {
     return <Appointment key={appointment.id} {...appointment} />
