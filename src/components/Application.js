6e3c399b725207c740401b072099711b9a9b9889
dikshantkunwar <dikshantkunwar@gmail.com>
2022-01-07 00:04:30 -0500

    imported getinterview into application

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment/index";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
