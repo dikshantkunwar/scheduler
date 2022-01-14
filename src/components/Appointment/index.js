0b67abf0ab9e3adaa1ff28479d85508ece5b8160
dikshantkunwar <dikshantkunwar@gmail.com>
2022-01-14 13:38:42 -0500

    added events invoking hook methods to switch between states

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,37 @@ import React from "react"
 import Empty from "./Empty"
 import Header from "./Header"
 import Show from "./Show"
+import useVisualMode from "hooks/useVisualMode"
 import "./styles.scss"
+import Form from "./Form"
+
+// Mode constants 
+const EMPTY = "EMPTY",
+      SHOW = "SHOW",
+      CREATE = "CREATE";
 
 export default function Appointment(props) {
-  const {time, interview} = props
+  const { time, interview } = props
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  )
+  const interviewers = [];
   return(
     <article className="appointment">
       <Header id={props.id} time={time} />
-      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student = {interview.student}
+          interviewer = {interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers = {interviewers}
+          onCancel = {() => back()}
+        />
+      )}
     </article>
   )
 }
