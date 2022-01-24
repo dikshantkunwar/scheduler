c5d1ea5147cdc588477f1b527324e1bcb9a89e10
dikshantkunwar <dikshantkunwar@gmail.com>
2022-01-24 14:24:52 -0500

    refactored useState to use useReducers instead

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,13 +1,51 @@
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import axios from 'axios'
+
+function reducer(state, action) {
+  switch(action.type){
+    case "SET_DAY": 
+      return {
+        ...state, 
+        day: action.value.day
+      };
+    case "SET_APPLICATION_DATA":
+      return {
+        ...state,
+        days: action.value.days,
+        appointments: action.value.appointments,
+        interviewers: action.value.interviewers
+      }
+    case "SET_INTERVIEW":
+      const appointment = {
+        ...state.appointments[action.value.id],
+        interview : {...action.value.interview}
+      }
+      const appointments = {
+        ...state.appointments,
+        [action.value.id]: appointment
+      };
+
+      const days = [...state.days];
+      return {
+        ...state,
+        appointments,
+        days
+      }
+  }
+  if (action.type === "setDay") {
+
+  }
+}
 
 export default function useApplicationData() {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  });
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   appointments: {}
+  // });
+  const initialState = {day: "Monday", days: [], appointments: {}}
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     Promise.all([
@@ -16,45 +54,50 @@ export default function useApplicationData() {
       axios.get('/api/interviewers')
     ]).then((all) => {
       const [days, appointments, interviewers] = all;
-      setState(prev => ({...prev, days: days.data, appointments: appointments.data, interviewers: interviewers.data}));
+      // setState(prev => ({...prev, days: days.data, appointments: appointments.data, interviewers: interviewers.data}));
+      dispatch({
+         type: "SET_APPLICATION_DATA", 
+         value: {days: days.data, appointments: appointments.data, interviewers: interviewers.data}
+        });
     });
   }, []);
 
-  const setDay = day => setState(prev => ({ ...prev, day }));
+  // const setDay = day => setState(prev => ({ ...prev, day }));
+  const setDay = day => dispatch({ type: "SET_DAY", value: day });
 
   function bookInterview(id, interview) {
     // Change local state of interview when an interview is booked
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+    // const appointment = {
+    //   ...state.appointments[id],
+    //   interview: { ...interview }
+    // };
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id]: appointment
+    // };
     
-    return axios.put(`/api/appointments/${id}`, { ...appointment })
+    return axios.put(`/api/appointments/${id}`, { interview })
     .then(res => {
       updateSpots("create");
       const days = [...state.days]  //update days state to change the spots 
-      setState({ ...state, appointments, days }); 
+      // setState({ ...state, appointments, days }); 
+      dispatch({
+        type: "SET_INTERVIEW",
+        value: {id: id, interview: interview}
+      });
     });
   }
 
   function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
     return axios.delete(`api/appointments/${id}`, { interview: null })
     .then(res => {
       updateSpots("delete");
-      const days = [...state.days]
-      setState({ ...state, appointments, days });
+      // const days = [...state.days]
+      // setState({ ...state, appointments, days });
+      dispatch({
+        type: "SET_INTERVIEW",
+        value: {id: id, interview: null}
+      });
     })
   }
 
