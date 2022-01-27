75c98591eb1ec4ae0e51bd9f77e613a962acc20d
dikshantkunwar <dikshantkunwar@gmail.com>
2022-01-26 19:14:52 -0500

    updated form test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Application from "components/Application";
 
+afterEach(cleanup);
+
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Application />);
